027dd543a06d4671ece4c893a78b39dc56e2ca0c
laurenlu26 <laurenncrews@gmail.com>
2022-05-18 22:27:07 -0700

    weather icon

--- index.js
+++ index.js
@@ -34,6 +34,12 @@ function displayCurrentWeather(response) {
   document.querySelector("#low").innerHTML = Math.round(
     response.data.main.temp_min
   );
+  let iconElement = document.querySelector("#icon");
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
   celsiusTemperature = response.data.main.temp;
 
   getForecast(response.data.coord);
