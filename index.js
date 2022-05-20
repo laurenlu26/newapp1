e1ff74c9cbda68b6df713d6a76c2facedc15f06f
laurenlu26 <laurenncrews@gmail.com>
2022-05-20 00:11:07 -0700

    icon and source code

--- index.js
+++ index.js
@@ -34,14 +34,14 @@ function displayCurrentWeather(response) {
   document.querySelector("#low").innerHTML = Math.round(
     response.data.main.temp_min
   );
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
   let iconElement = document.querySelector("#icon");
   iconElement.setAttribute(
     "src",
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
-  iconElement.setAttribute("alt", response.data.weather[0].description);
-  celsiusTemperature = response.data.main.temp;
-
   getForecast(response.data.coord);
 }
 function getForecast(coordinates) {
